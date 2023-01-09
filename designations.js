290335e44ebebea88b6e35671564147afcfe64e5
Seafoxlrt616 <103425286+Seafoxlrt616@users.noreply.github.com>
2023-01-10 00:33:10 +0800

    Update designations.js

--- designations.js
+++ designations.js
@@ -470,6 +470,17 @@ DesignationSystem.westernPacific = new DesignationSystem({
     ]
 });
 
+DesignationSystem.westernPacific2 = new DesignationSystem({
+    displayName: 'Western Pacific (Custom)',
+    suffix: 'W',
+    mainLists: [
+        ["Alvin","Becky","Clement","Diana","Eric","Fiona","Greg","Helen","Isaac","Jessica","Kenny","Lisa","Marco","Nicole","Otto","Phoebe","Robert","Sophia","Tony","Viola","Walter","Yvonne","Zeb"],
+        ["Amelia","Bosco","Cheryl","Derek","Evelyn","Frankie","Gloria","Harold","Ida","Joshua","Kate","Lenny","Maggie","Nathan","Olga","Philip","Rebecca","Stephen","Tanya","Vince","Wendy","York","Zia"],
+        ["Albert","Bonnie","Colin","Dora","Ernesto","Felicia","Gordon","Harriet","Ivan","Judy","Kyle","Lisa","Mike","Nancy","Owen","Percy","Ryan","Sharon","Thomas","Virginie","Wayne","Yates","Zachary"],
+        ["Alison","Billy","Chloe","Darren","Elsie","Felix","Garnet","Hugo","Isabel","Jack","Koryn","Liam","Maria","Nolan","Ophelia","Preston","Rina","Seth","Teresa","Victor","Wilda","Yago","Zoe"]
+    ]
+});
+
 DesignationSystem.PAGASA = new DesignationSystem({
     displayName: 'PAGASA',
     secondary: true,
@@ -626,6 +637,7 @@ DesignationSystem.presetDesignationSystems = [
     DesignationSystem.easternPacific,
     DesignationSystem.centralPacific,
     DesignationSystem.westernPacific,
+    DesignationSystem.westernPacific2,
     DesignationSystem.PAGASA,
     DesignationSystem.northIndianOcean,
     DesignationSystem.australianRegionBoM,
